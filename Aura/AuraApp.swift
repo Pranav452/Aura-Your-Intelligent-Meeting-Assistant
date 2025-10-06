cdb613a42f38ca76b12d9da76496a9f70d888806
Pranav Nair <pranavnairop090@gmail.com>
2025-10-06 10:22:03 +0530

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by Pranav Nair on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraTests/AuraTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuraTests.swift
+//  AuraTests
+//
+//  Created by Pranav Nair on 10/6/25.
+//
+
+import Testing
+@testable import Aura
+
+struct AuraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
